1d47995c5791b359771b9d794b71aef34fde4c92
Jack Dunne <57883799+JackDunne5877@users.noreply.github.com>
2022-04-09 17:23:24 -0400

    update test

--- application[1].py
+++ application[1].py
@@ -8,7 +8,7 @@ application.config['SESSION_COOKIE_NAME'] = 'User Cookie'
 
 @application.route('/')
 def index():
-	return "Hello Kevin, Vinayak, Guo, and Jack! This is additional message0"
+	return "Hello Kevin, Vinayak, Guo, and Jack! This is additional message. Testing out a new change via git"
 
 
 if __name__ == '__main__':
