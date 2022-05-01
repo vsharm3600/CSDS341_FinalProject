4eeeab6399c0149f6f4fcf16abd3177535dc04b7
Jack Dunne <57883799+JackDunne5877@users.noreply.github.com>
2022-05-01 15:19:44 -0400

    final updates for build database file

--- build-covid-tracker-DB.py
+++ build-covid-tracker-DB.py
@@ -7,7 +7,7 @@ create_DB_query = "CREATE DATABASE covid_tracker_DB"
 
 #IMPORTANT(PLEASE READ): If you are trying to build a localhosted database use this code. Otherwise DO NOT run this file!
 create_Student_table_query = """
-CREATE TABLE Student(
+CREATE TABLE Students(
 	sid INT PRIMARY KEY,
 	Address VARCHAR(100)
 )
@@ -112,44 +112,45 @@ try:
 		host="localhost",
 		user=input("Enter username: "),
 		password=getpass("Enter password: "),
+		database="covid_tracker_DB"
 	) as connection:
 		with connection.cursor() as cursor:
-			cursor.execute(create_DB_query)
+			#cursor.execute(create_DB_query)
 			cursor.execute(create_Student_table_query)
 			for i,row in df_stud.iterrows():
             			sql = "INSERT INTO covid_tracker_DB.Student VALUES (%s,%s)"
             			cursor.execute(sql, tuple(row))
-            			cursor.commit()
+            			#cursor.commit()
 			cursor.execute(create_Section_table_query)
 			for i,row in df_sect.iterrows():
             			sql = "INSERT INTO covid_tracker_DB.Section VALUES (%s,%s)"
             			cursor.execute(sql, tuple(row))
-            			cursor.commit()
+            			#cursor.commit()
 			cursor.execute(create_Orgainzations_table_query)
 			for i,row in df_org.iterrows():
             			sql = "INSERT INTO covid_tracker_DB.Organizations VALUES (%s,%s,%s)"
             			cursor.execute(sql, tuple(row))
-            			cursor.commit()
+            			#cursor.commit()
 			cursor.execute(create_Tests_table_query)
 			for i,row in df_tests.iterrows():
             			sql = "INSERT INTO covid_tracker_DB.Tests VALUES (%s,%s)"
             			cursor.execute(sql, tuple(row))
-            			cursor.commit()
+            			#cursor.commit()
 			cursor.execute(create_Vaccination_table_query)
 			for i,row in df_vax.iterrows():
             			sql = "INSERT INTO covid_tracker_DB.Vaccination VALUES (%s,%s,%s,%s)"
             			cursor.execute(sql, tuple(row))
-            			cursor.commit()
+            			#cursor.commit()
 			cursor.execute(create_Enrolled_table_query)
 			for i,row in df_enroll.iterrows():
             			sql = "INSERT INTO covid_tracker_DB.Enrolled VALUES (%s,%s)"
             			cursor.execute(sql, tuple(row))
-            			cursor.commit()
+            			#cursor.commit()
 			cursor.execute(create_Participates_table_query)
 			for i,row in df_part.iterrows():
             			sql = "INSERT INTO covid_tracker_DB.Participates VALUES (%s,%s)"
             			cursor.execute(sql, tuple(row))
-            			cursor.commit()
+            			#cursor.commit()
 			connection.commit()
 except Error as e:
 	print(e)
